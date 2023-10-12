86898eec82b9e4691d9b078130ab756cca56a359
dawid <dawidp789@gmail.com>
2023-10-12 14:04:02 +0200

    file service

---
+++ booking-service/src/main/java/com/dpap/bookingapp/storage/files/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.dpap.bookingapp.storage.files;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ booking-service/src/main/java/com/dpap/bookingapp/storage/files/UploadFileResponse.java
@@ -0,0 +1,10 @@
+package com.dpap.bookingapp.storage.files;
+
+public record UploadFileResponse (
+     String fileName,
+     String fileDownloadUri,
+     String fileType,
+     long size
+){
+
+}
